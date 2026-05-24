398c79f911028ebcfcaab89e5fe1266cd2c1c033
Alexandre Nunes <alexandrenunesmartins@hotmail.com>
2026-05-24 10:47:49 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace QuickGridDemo.Models;
+public class Car
+{
+    public int Id { get; set; }
+    public string Manufacturer { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public string Year { get; set; } = string.Empty;
+}
