ab7000c6495ad44ed9e501dcf1bceef9be781dce
Alexandre Martins <alexandre.martins@aspec.com.br>
2026-06-01 15:04:17 -0300

    Habilita validação por DataAnnotations

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,8 +1,16 @@
-﻿namespace QuickGridDemo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickGridDemo.Models;
 public class Car
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Fabricante é obrigatório")]
     public string Manufacturer { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Modelo é obrigatório")]
     public string Model { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Ano é obrigatório")]
     public string Year { get; set; } = string.Empty;
 }
